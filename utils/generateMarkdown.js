058ecc641e145d74d64a8a48fbafacac547cdf80
Gatis Jansons <bobsters.lol@gmail.com>
2024-02-18 13:07:17 +0000

    markdown generator created

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,41 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${data.description}
+
+  ## Installation
+
+  \`\`\`bash
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+  \`\`\`bash
+  ${data.usage}
+  \`\`\`
+
+  ## Tests
+  \`\`\`bash
+  ${data.test}
+  \`\`\`
+
+
+  ## Contributing
+  ${data.contributing}
+
+
+
+
+
+  ## License
+  [${data.license}](https://choosealicense.com/licenses/${data.license})
+
+
+  ## Questions
+
+  If you have Any question please don't hesitate to contact
+ (mailto:${data.email}) or reach out to me in https://github.com/${data.githubUsername}
+
 `;
 }
 
